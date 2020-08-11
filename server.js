b09a656e93bd1d760ddfe5d7e8a25de6a1075965
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-08-11 15:43:07 -0600

    Add schema variable

--- server.js
+++ server.js
@@ -2,3 +2,8 @@ var express = require('express');
 var express_graphql = require('express-graphql');
 var { buildschema } = require('graphql');
 
+var schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
