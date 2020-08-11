56c62d9bdf4ae2a09277ae64fa8eba776bb018af
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-08-11 15:44:07 -0600

    Add express app functionality

--- server.js
+++ server.js
@@ -12,3 +12,10 @@ var root = {
   message: () => 'Hello World'
 };
 
+var app = express;
+app.use('/graphql', express_graphql({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
