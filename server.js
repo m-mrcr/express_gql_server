2034efaffd84126f05ad2c474c19d3cec4c263df
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-08-11 16:21:52 -0600

    Adjust buildSchema capitalization

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');// GraphQL schema
+var { buildSchema } = require('graphql');
+
 var schema = buildSchema(`
     type Query {
         message: String
     }
-`);// Root resolver
+`);
+
 var root = {
-    message: () => 'Hello World!'
-};// Create an express server and a GraphQL endpoint
-var app = express();
+  message: () => 'Hello World'
+};
+
+var app = express;
 app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true
+  schema: schema,
+  rootValue: root,
+  graphiql: true
 }));
 app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
